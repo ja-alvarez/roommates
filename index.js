d464c59870e72ac4f1ad404747dc54654c82123b
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-06-30 00:48:49 -0400

    correcion endpoint post /gasto, se agregan rutas faltantes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const log = console.log;
 const port = 3000;
-const print = console.log;
+
 // MIDDLEWARES GENERALES
 app.use(express.json());
 app.use(morgan("tiny"));
@@ -29,14 +29,13 @@ if (!fs.existsSync('roommates.json')) {
     fs.writeFileSync('roommates.json', JSON.stringify(roommates, null, 2), 'utf8');
 }
 
-
-
 //RUTA PÁGINA PRINCIPAL
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './public/index.html'));
 });
 
 // ENDPOINTS
+// Almacenar nuevo roommate usando random user
 app.post('/roommate', async (req, res) => {
     try {
         const getUser = async () => {
@@ -53,7 +52,6 @@ app.post('/roommate', async (req, res) => {
                 data.roommates.push(roommate);
                 fs.writeFileSync('roommates.json', JSON.stringify(data, null, 2));
                 log('Nuevo roommate almacenado con éxito.')
-                print('Hola Mundo!')
                 return roommate;
             } catch (error) {
                 console.error(error);
@@ -67,39 +65,68 @@ app.post('/roommate', async (req, res) => {
     }
 });
 
+// Devolver todos los roommates almacenados
 app.get('/roommates', async (req, res) => {
     try {
         const data = JSON.parse(fs.readFileSync('roommates.json', 'utf8'));
         res.json(data);
-        //res.json
     } catch (error) {
         const message = error.message
         res.status(400).json({ message })
     }
 });
 
-app.post('/gasto', async (roommate, descripcion, monto) => {
+// Devuelve los gastos registrados en gastos.json
+app.get('/gastos', async (req, res) => {
     try {
-
-
-        const gasto = { roommateSelected: roommate, descripcion, monto };
         const data = JSON.parse(fs.readFileSync('gastos.json', 'utf8'));
-        if (!Array.isArray(data.gasto)) {
-            data.gastos = [];
-        }
-        data.gastos.push(gasto);
-        fs.writeFileSync('roommates.json', JSON.stringify(data, null, 2));
-        log('Nuevo gasto almacenado con éxito.')
-        res.status(201).json(data)
-
-
-
+        res.json(data);
     } catch (error) {
-        res.send(error)
+        res.status(400).json({ message: 'Error al leer el archivo de gastos', error: error.message });
     }
 });
 
+// Recibe el payload con los datos del gasto y almacena en gastos.json
+app.post('/gasto', async (req, res) => {
+    try {
+        const { roommate, descripcion, monto } = req.body
+        const id = uuidv4().slice(0, 6);
+        console.log(req.body);
+        log('** Roommate: ', roommate, 'descripcion: ', descripcion, 'monto: ', monto);
+        const gasto = { id, roommate, descripcion, monto };
+        const data = JSON.parse(fs.readFileSync('gastos.json', 'utf8'));
+        if (!Array.isArray(data.gastos)) {
+            data.gastos = [];
+        }
+        data.gastos.push(gasto);
+        fs.writeFileSync('gastos.json', JSON.stringify(data, null, 2));
+        log('Nuevo gasto almacenado con éxito.')
+        res.status(201).json(gasto)
+    } catch (error) {
+        res.status(500).json({ message: 'Error al almacenar nuevo gasto.', error: error.message });
+    }
+});
 
+// Elimina un gasto del historial
+app.delete('/gasto', (req, res) => {
+    const { id } = req.query;
+    if (id) {
+        const data = JSON.parse(fs.readFileSync('gastos.json', 'utf8'));
+        const gastos = data.gastos
+        const index = gastos.findIndex(g => g.id === id)
+        if (index !== -1) {
+            try {
+                gastos.splice(index, 1)
+                fs.writeFileSync('gastos.json', JSON.stringify(data, null, 2));
+                res.status(200).json({message: `Gasto eliminado con éxito.`});
+            } catch (error) {
+                res.status(500).json({message: 'Error al eliminar el gasto.'});
+            }
+        } else {
+            res.status(400).json({message: 'Gasto no encontrado o no proporcionado.'});
+        }
+    }
+});
 
 app.all('*', (req, res) => {
     res.send('Página no encontrada.')
